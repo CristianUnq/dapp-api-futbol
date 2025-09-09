fe338cefface5711d4a6be0d063b9b64edd30582
CristianUnq <liotinecristian@hotmail.com>
2025-09-09 01:07:07 -0300

    primer push

---
+++ src/main/java/com/dapp/api_futbol/ApiFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.dapp.api_futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dapp/api_futbol/ApiFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapp.api_futbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

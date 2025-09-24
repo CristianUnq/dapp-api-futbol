6156e0d537dde1e9176c8bd6504b167e2cfc0874
CristianUnq <liotinecristian@hotmail.com>
2025-09-23 23:47:20 -0300

    test no se conectan a la base

--- src/test/java/com/dapp/api_futbol/ApiFutbolApplicationTests.java
+++ src/test/java/com/dapp/api_futbol/ApiFutbolApplicationTests.java
@@ -1,9 +1,12 @@
 package com.dapp.api_futbol;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class ApiFutbolApplicationTests {
 
 	@Test

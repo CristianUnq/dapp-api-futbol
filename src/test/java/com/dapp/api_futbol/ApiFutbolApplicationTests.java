004e8ffeceae03dedcad97f84e6be8054f839df8
CristianUnq <liotinecristian@hotmail.com>
2025-10-08 02:49:08 -0300

    borro WhoScoreService

--- src/test/java/com/dapp/api_futbol/ApiFutbolApplicationTests.java
+++ src/test/java/com/dapp/api_futbol/ApiFutbolApplicationTests.java
@@ -4,13 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import com.dapp.api_futbol.service.WhoScoreService;
-
 @SpringBootTest
 class ApiFutbolApplicationTests {
-
-	@MockitoBean
-    private WhoScoreService whoScoreService;
 
 	@Test
 	void contextLoads() {
